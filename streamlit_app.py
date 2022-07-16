c7b4114427cc6f6905ec0e494d56cab586a68329
pierswalker71 <60096809+pierswalker71@users.noreply.github.com>
2022-07-16 14:37:39 +0100

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+
+def main():
+    # Imports
+    
+   import pandas as pd
+   import re 
+    
+    # Settings
+    st.set_page_config(page_title = 'Sentiment Analysis') 
+    
+    # Title
+    st.title('Sentiment Analysis - Restaurant Reviews')
+    st.write('Piers Walker 2022. https://github.com/pierswalker71')
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+if __name__ == '__main__':
+    main()
