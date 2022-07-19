0217a5eaf680adfcd8f76cbf1a073b994ab82955
pierswalker71 <60096809+pierswalker71@users.noreply.github.com>
2022-07-19 15:09:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+#95f09517
 def main():
     
     #==============================================================================
@@ -57,11 +57,6 @@ def main():
     #==============================================================================
     # Functions 
     
-    @st.cache 
-    def load_csv(file_name, delimiter='\t'):
-        df = pd.read_csv(file_name, delimiter=delimiter)
-        return df
-                
     def lemmatization(text_list, en, stopwords):
         # Creates a corpus of text from a list of sentences
         
@@ -88,7 +83,7 @@ def main():
                 binary_values.append(1)
         return binary_values
     #-----------------------------------------------------------------------------    
-    @st.cache(allow_output_mutation=True) # It is run twice for training an operational  
+    
     def build_classifier(model_type='Logistic Regression', keras_input_dimensions=1000):
         # Returns a classified based on the desired type
         # Input: model_type(str): Selects model. One of 'Logistic Regression', 'Bernoulli Naive Bayes', 'Neural Network'
@@ -109,45 +104,36 @@ def main():
             classifier.add(Dropout(0.5))
             classifier.add(Dense(1, activation="sigmoid"))
             classifier.compile(loss='binary_crossentropy', metrics='accuracy')   
+            
         return classifier   
-    
- 
-    def fit_classifier(classifier,X,y,epochs=20):   
-        if model_type == 'Neural Network':
-            classifier.fit(X, y, epochs=20,)    
-        else:
-            classifier.fit(X, y)   
-        return classifier
     
     #==============================================================================
 
-    #if st.session_state['data_loaded'] == False:
-    # Load data
-    st.header('Training data')          
-    input_data = load_csv(file_name='Restaurant_Reviews.tsv')
-        
-    with st.expander('Review data'):
-        st.dataframe(input_data)
-        pos = len(input_data[input_data['Liked']==1].index)
-        neg = len(input_data[input_data['Liked']==0].index)
-        st.write(f'Number of positive reviews = {pos} ({round(pos*100/(pos+neg),2)}%)')
-        st.write(f'Number of negative reviews = {neg} ({round(neg*100/(pos+neg),2)}%)')
+    if st.session_state['data_loaded'] == False:
+        # Load data
+        st.header('Training data')
+        input_data = pd.read_csv('Restaurant_Reviews.tsv',delimiter='\t')
+
+        with st.expander('Review data'):
+            st.dataframe(input_data)
+            pos = len(input_data[input_data['Liked']==1].index)
+            neg = len(input_data[input_data['Liked']==0].index)
+            st.write(f'Number of positive reviews = {pos} ({round(pos*100/(pos+neg),2)}%)')
+            st.write(f'Number of negative reviews = {neg} ({round(neg*100/(pos+neg),2)}%)')
      
-    # Process text data
-    # Create corpus of review text, removing stop words and other characters
-    text_list = input_data['Review']
-    corpus = lemmatization(text_list, en, stopwords)
+        # Process text data
+        # Create corpus of review text, removing stop words and other characters
+        text_list = input_data['Review']
+        corpus = lemmatization(text_list, en, stopwords)
     
-    # Convert the corpus of text items to a matrix of token counts (a bit like onehot encoding)
-    countvector = CountVectorizer()
-    X = countvector.fit_transform(corpus).toarray()
-      
-    y = input_data['Liked'].values
+        countvector = CountVectorizer()
+        X = countvector.fit_transform(corpus).toarray()
+        y = input_data['Liked'].values
     
-    # Train test split the data
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+        # Train test split the data
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
         
-    st.session_state['data_loaded'] = True
+        st.session_state['training_flag'] = True
     
     #==============================================================================
     # User selection of model type model 
@@ -156,50 +142,50 @@ def main():
         model_type = st.selectbox('Select model type', ['Logistic Regression','Naive Bayes', 'Bernoulli Naive Bayes','Neural Network'])
         
     #==============================================================================    
-    #if st.session_state['model_type'] != model_type:
-    # Build training model of selected type
-    classifier_training = build_classifier(model_type=model_type, keras_input_dimensions=X.shape[1])
+    if st.session_state['model_type'] != model_type:
+        # Build training model of selected type
+        classifier_training = build_classifier(model_type=model_type, keras_input_dimensions=X.shape[1])
 
-    if model_type == 'Neural Network': # Keras neural network        
-        classifier_training.fit(X_train, y_train, epochs=20,)    
-        continuous_values = classifier_training.predict(X_test)
-        y_pred = threshold_to_binary(continuous_values)
-    else:
-        classifier_training.fit(X_train, y_train)
-        y_pred = classifier_training.predict(X_test)     
+        if model_type == 'Neural Network': # Keras neural network        
+            classifier_training.fit(X_train, y_train, epochs=20,)    
+            continuous_values = classifier_training.predict(X_test)
+            y_pred = threshold_to_binary(continuous_values)
+        else:
+            classifier_training.fit(X_train, y_train)
+            y_pred = classifier_training.predict(X_test)     
 
-    #-----------------------------------------------------------------------------
-    # Assess training results
-    confusion_matrix = confusion_matrix(y_test, y_pred)
+        #-----------------------------------------------------------------------------
+        # Assess training results
+        confusion_matrix = confusion_matrix(y_test, y_pred)
     
-    acc = accuracy_score(y_test, y_pred)
-    prec = precision_score(y_test, y_pred)
-    recall = recall_score(y_test, y_pred)
-    auc = roc_auc_score(y_test, y_pred)
-    f1 = f1_score(y_test, y_pred)
-    with st.sidebar:
-        st.header('Model training performance')
-        st.write('confusion_matrix')
-        st.write(confusion_matrix)
-        st.write(f'Accuracy: {round(acc*100,2)} %')
-        st.write(f'Precision: {round(prec*100,2)} %')
-        st.write(f'Recall: {round(recall*100,2)} %')      
-      
-        st.write(f'Roc auc score: {round(auc*100,2)}')
-        st.write(f'f1 score: {round(f1*100,2)}')     
+        acc = accuracy_score(y_test, y_pred)
+        prec = precision_score(y_test, y_pred)
+        recall = recall_score(y_test, y_pred)
+        auc = roc_auc_score(y_test, y_pred)
+        f1 = f1_score(y_test, y_pred)
+        with st.sidebar:
+            st.header('Model training performance')
+            st.write('confusion_matrix')
+            st.write(confusion_matrix)
+            st.write(f'Accuracy: {round(acc*100,2)} %')
+            st.write(f'Precision: {round(prec*100,2)} %')
+            st.write(f'Recall: {round(recall*100,2)} %')      
+          
+            st.write(f'Roc auc score: {round(auc*100,2)}')
+            st.write(f'f1 score: {round(f1*100,2)}')     
         
-    # Update session state value with most recently trained model type
-    #st.session_state['model_type'] = model_type
+        # Update session state value with most recently trained model
+        st.session_state['model_type'] = model_type
     
-    #==============================================================================
-    #if st.session_state['model_type'] != model_type:
-    # Rebuild classifier and train on whole dataset
-    classifier = build_classifier(model_type=model_type, keras_input_dimensions=X.shape[1])
-    # Add any optimal hyperparameters here
+        #==============================================================================
+        # Rebuild classifier and train on whole dataset
+        classifier = build_classifier(model_type=model_type, keras_input_dimensions=X.shape[1])
+        # Add any optimal hyperparameters here
     
-    classifier = fit_classifier(classifier, X, y, model_type='Logistic Regression', epochs=20)
-        
-
+        if model_type == 'Neural Network':
+            classifier.fit(X, y, epochs=20,)    
+        else:
+            classifier.fit(X, y)     
     
     #-----------------------------------------------------------------------------
     # Make prediction using user entered review text
@@ -210,11 +196,23 @@ def main():
          
     # Make prediction
     prediction = classifier.predict(countvector.transform(text_spacy))
+    
+    # Convert continuous vaues to binary if required
+    if model_type == 'Neural Network':
+        continuous_values = prediction
+        
+        binary_values = []
+        for i in continuous_values:
+            if i[0] < 0.5:
+                binary_values.append(0)
+            else:
+                binary_values.append(1)
+        prediction = binary_values
         
     st.header('Evaluation')    
     st.write(f'key word components found in your review: [{text_spacy[0]}]') 
     st.write('\nMy prediction:')
-    if prediction >0.5:
+    if prediction == 1:
         st.markdown('**I think this is a positive review comment**')
     else:
         st.markdown('**I think this is a negative review comment**')
